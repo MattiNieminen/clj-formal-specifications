90ba7399e52e3f152c7fe847ff5b2cef0037b06b
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2014-12-24 20:55:03 +0200

    Updated project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-formal-specifications "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for creating executable formal
+  specifications."
+  :url "https://github.com/MattiNieminen/clj-formal-specifications"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
