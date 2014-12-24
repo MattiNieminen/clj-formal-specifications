1c007a3e8ecdb94a7d6a145897f9e597b28e3a3b
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2014-12-24 20:47:46 +0200

    Created project with Leiningen.

---
+++ test/clj_formal_specifications/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-formal-specifications.core-test
+  (:require [clojure.test :refer :all]
+            [clj-formal-specifications.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
