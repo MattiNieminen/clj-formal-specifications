65b26f4b2ff079d32f8b8bce057e4ad493b57c84
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-01-03 00:48:02 +0200

    Added first versions of functions for executing formal specification actions.

--- src/clj_formal_specifications/core.clj
+++ src/clj_formal_specifications/core.clj
@@ -1,6 +1,25 @@
 (ns clj-formal-specifications.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn action?
+  [action]
+  (contains? action :body))
+
+(defn available?
+  [action]
+  (if (contains? action :available) (:available action) true))
+
+(defn- test-action
+  [action]
+  (cond
+     (not (action? action))
+       (throw (Exception. "given parameter is not a valid action."))
+
+     (not (available? action))
+       (throw (Exception. "action is not available for execution"))
+
+     :else true))
+
+(defn execute
+  [action]
+  (if (test-action action) (:body action)))
+
