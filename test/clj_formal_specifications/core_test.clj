e3cbb4c1e93b768e2cfcc8a45bb119f303fc7ca3
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-01-03 15:03:40 +0200

    Added tests for functions related to executing formal specifications.

--- test/clj_formal_specifications/core_test.clj
+++ test/clj_formal_specifications/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [clj-formal-specifications.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest action?-test
+  (is (true? (action? {:body (+ 1 1)})))
+  (is (false? (action? {:a 1 :b 2}))))
+
+(deftest available?-test
+  (testing "without specified availability"
+    (is (true? (available? {:body (+ 1 1)}))))
+  (testing "with availability specified"
+    (is (true? (available? {:available true :body (+ 1 1)})))
+    (is (false? (available? {:available false :body (+ 1 1)})))))
+
+(deftest execute-test
+  (testing "with proper actions"
+    (is (= 2 (execute {:body (+ 1 1)})))
+    (is (= 2 (execute {:body (+ 1 1) :available true}))))
+  (testing "with actions without executable body"
+    (is (thrown? Exception (execute {:available true})))
+    (is (thrown? Exception (execute {:available true :no-body (+ 1 1)})))
+    (is (thrown? Exception (execute {:no-body (+ 1 1)}))))
+  (testing "with unavailable actions"
+    (is (thrown? Exception (execute {:available false :body (+ 1 1)})))))
