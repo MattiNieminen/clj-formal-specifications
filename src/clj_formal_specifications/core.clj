2f5a8c70f0b5d5ecbff4e709e1c34edae7420d95
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-01-03 15:13:57 +0200

    Added documentation strings to core functions.

--- src/clj_formal_specifications/core.clj
+++ src/clj_formal_specifications/core.clj
@@ -1,14 +1,18 @@
 (ns clj-formal-specifications.core)
 
 (defn action?
+  "Returns true if action is a valid action with body."
   [action]
   (contains? action :body))
 
 (defn available?
+  "Returns true if action is available for execution."
   [action]
   (if (contains? action :available) (:available action) true))
 
 (defn- test-action
+  "Throws exceptions if action can not be executed. Used to give reasonable
+  error messages for why executing an action has failed."
   [action]
   (cond
      (not (action? action))
@@ -20,6 +24,7 @@
      :else true))
 
 (defn execute
+  "Executes action body and returns the evaluated value if an action is
+  available and well-formed."
   [action]
   (if (test-action action) (:body action)))
-
