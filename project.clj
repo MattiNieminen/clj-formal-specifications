f984ef602f90e1ff62f64aecb7e9f96e0ddc3a86
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-02-06 21:34:13 +0200

    Added deploy-repositories setting to project file.

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :url "https://github.com/MattiNieminen/clj-formal-specifications"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :sign-releases false}]])
